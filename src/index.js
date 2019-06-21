7b5807b9e11a1779f756aeca96f520f5441be756
CaioQuirinoMedeiros <48543208+CaioQuirinoMedeiros@users.noreply.github.com>
2019-06-21 00:31:31 -0300

    Setting up .gitignore and .env

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const server = require("http").Server(app);
 const io = require("socket.io")(server);
 
 mongoose.connect(
-  "mongodb+srv://caioquirino:pedemanga@instagramdb-3ga5p.mongodb.net/test?retryWrites=true&w=majority",
+  process.env.MONGO_CONNECT_URL,
   {
     useNewUrlParser: true
   }
