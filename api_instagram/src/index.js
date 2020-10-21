aac81c2b8fee5c000a978515fb3b8d7b1af96338
Caio Quirino Medeiros <caio.quirino.medeiros@gmail.com>
2020-10-20 23:15:13 -0300

    improves on api server

--- api_instagram/src/index.js
+++ api_instagram/src/index.js
@@ -10,7 +10,8 @@ const server = require("http").Server(app);
 const io = require("socket.io")(server);
 
 mongoose.connect(process.env.MONGO_CONNECT_URL, {
-  useNewUrlParser: true
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
 app.use((req, res, next) => {
@@ -28,4 +29,8 @@ app.use(
 
 app.use(require("./routes"));
 
-server.listen(process.env.PORT || 3333);
+const PORT = process.env.PORT || 3333;
+
+server.listen(PORT, () => {
+  console.log(`server running on port ${PORT}`);
+});
